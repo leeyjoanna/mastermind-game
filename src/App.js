ace172518925b7309148db05c4ace44b0ea7a337
Joanna <leejoanna94@gmail.com>
2021-11-13 11:39:03 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header.js'
+import Main from './components/Main.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
